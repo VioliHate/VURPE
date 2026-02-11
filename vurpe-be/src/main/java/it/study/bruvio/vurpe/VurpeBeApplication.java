7c41b58a3974ff7a3225e1bea52a2f2edd641fd3
Domenico Violi <dome.violi90@gmail.com>
2026-02-11 20:58:42 +0100

    update
    start and setup project vurpe-be

---
+++ vurpe-be/src/main/java/it/study/bruvio/vurpe/VurpeBeApplication.java
@@ -0,0 +1,13 @@
+package it.study.bruvio.vurpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VurpeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VurpeBeApplication.class, args);
+	}
+
+}
---
+++ vurpe-be/src/test/java/it/study/bruvio/vurpe/VurpeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.study.bruvio.vurpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VurpeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

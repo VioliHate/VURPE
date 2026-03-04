0ade684284c1e92504971917d28aef44643da9ab
Domenico Violi <dome.violi90@gmail.com>
2026-03-04 12:44:34 +0100

    Update VurpeBeApplication.java
    fix warning

--- vurpe-be/src/main/java/it/study/bruvio/vurpe/VurpeBeApplication.java
+++ vurpe-be/src/main/java/it/study/bruvio/vurpe/VurpeBeApplication.java
@@ -2,8 +2,10 @@ package it.study.bruvio.vurpe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class VurpeBeApplication {
 
 	public static void main(String[] args) {

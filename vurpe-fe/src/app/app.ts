a1d50d55193f799ca074b67bc566b0a6f331cf4b
Domenico Violi <dome.violi90@gmail.com>
2026-03-10 18:12:30 +0100

    update

--- vurpe-fe/src/app/app.ts
+++ vurpe-fe/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
 import {MatListItem, MatNavList} from '@angular/material/list';
 import {MatIcon} from '@angular/material/icon';
@@ -7,13 +7,19 @@ import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavContent, MatSidenav, MatSidenavContainer, MatNavList, MatIcon, MatListItem, MatIconButton],
+  imports: [RouterOutlet, MatSidenavContent, MatSidenav, MatSidenavContainer, MatNavList, MatIcon, MatListItem, MatIconButton, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('vurpe-fe');
-  expanded = true;   // parte aperto in versione estesa
+  expanded = true;
+
+  listMenu = [
+    {route: '/dashboard', title: 'Dashboard', icon: 'dashboard'},
+    {route: '/users', title: 'Utenti', icon: 'people'},
+    {route: '/settings', title: 'Impostazioni', icon: 'settings'},
+  ];
 
   toggleExpand() {
     this.expanded = !this.expanded;

f191110020a3f9cd8e5333c6190f184c87074d09
Domenico Violi <dome.violi90@gmail.com>
2026-03-10 17:51:47 +0100

    update interface

--- vurpe-fe/src/app/app.ts
+++ vurpe-fe/src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavContent, MatSidenav, MatSidenavContainer, MatNavList, MatIcon, MatListItem, MatIconButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('vurpe-fe');
+  expanded = true;   // parte aperto in versione estesa
+
+  toggleExpand() {
+    this.expanded = !this.expanded;
+  }
+
 }

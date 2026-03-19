ddc6d882b835c9b8d618dad4fbd2ee356d7a9392
Giuseppe Zoltan Bruniani <81922012+brngpp@users.noreply.github.com>
2026-03-19 15:00:28 +0100

    migliorie

--- vurpe-be/src/main/java/it/study/bruvio/vurpe/service/DashboardService.java
+++ vurpe-be/src/main/java/it/study/bruvio/vurpe/service/DashboardService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import it.study.bruvio.vurpe.dto.response.StatusCountDTO.DashBoardStatsDTO;
 import it.study.bruvio.vurpe.repository.AsyncTaskRepository;
+import it.study.bruvio.vurpe.repository.DataRecordRepository;
 import it.study.bruvio.vurpe.repository.FilesRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -12,11 +13,13 @@ import lombok.RequiredArgsConstructor;
 public class DashboardService {
     private final AsyncTaskRepository taskRepo;
     private final FilesRepository fileRepo;
+    private final DataRecordRepository dataRecordRepo;
 
     public DashBoardStatsDTO getDashboardMetrics() {
         DashBoardStatsDTO dto = new DashBoardStatsDTO();
         dto.setTasksStats(taskRepo.countTasksByStatus());
         dto.setFilesStats(fileRepo.countFilesByStatus());
+        dto.setDataRecordStats(dataRecordRepo.countDataRecordsByRiskFlag());
         return dto;
     }
 }

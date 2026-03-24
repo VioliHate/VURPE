af5fbacedbd9a1b25244b2e3b3369c813abd1b71
Domenico Violi <dome.violi90@gmail.com>
2026-03-24 12:27:06 +0100

    Update app.config.ts
    add provider

--- vurpe-fe/src/app/app.config.ts
+++ vurpe-fe/src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { routes } from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes,withComponentInputBinding()),
-    provideHttpClient()
-  ]
+    provideRouter(routes, withComponentInputBinding()),
+    provideCharts(withDefaultRegisterables()),
+    provideHttpClient(),
+  ],
 };

a97f3185f0d774ec505c96a65f5db9be0fa8074d
Domenico Violi <dome.violi90@gmail.com>
2026-03-26 12:59:48 +0100

    update UX

--- vurpe-fe/src/app/app.ts
+++ vurpe-fe/src/app/app.ts
@@ -1,38 +1,45 @@
 import { Component, inject, signal } from '@angular/core';
-import {NavigationEnd, Router, RouterLink, RouterOutlet} from '@angular/router';
-import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
-import {MatListItem, MatNavList} from '@angular/material/list';
-import {MatIcon} from '@angular/material/icon';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
 import { MatIconButton, MatAnchor } from '@angular/material/button';
 import { NavigationService } from './services/navigation-service';
 
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavContent, MatSidenav, MatSidenavContainer, MatNavList, MatIcon, MatListItem, MatIconButton, RouterLink, MatAnchor],
+  imports: [
+    RouterOutlet,
+    MatSidenavContent,
+    MatSidenav,
+    MatSidenavContainer,
+    MatNavList,
+    MatIcon,
+    MatListItem,
+    MatIconButton,
+    RouterLink,
+    MatAnchor,
+    RouterModule,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   private nav = inject(NavigationService);
   protected readonly title = signal('vurpe-fe');
   expanded = true;
 
-
   listMenu = [
-    {route: '/dashboard', title: 'Dashboard', icon: 'dashboard'},
-    {route: '/files', title: 'File', icon: 'insert_drive_file'},
-    {route: '/rules', title: 'Rules', icon: 'rule'},
+    { route: '/dashboard', title: 'Dashboard', icon: 'dashboard' },
+    { route: '/files', title: 'File', icon: 'insert_drive_file' },
+    { route: '/rules', title: 'Rules', icon: 'rule' },
   ];
-
-    
 
   toggleExpand() {
     this.expanded = !this.expanded;
   }
-  
-goBack(){
-  this.nav.back();
-}
 
+  goBack() {
+    this.nav.back();
+  }
 }

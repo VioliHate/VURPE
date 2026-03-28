77060d38d1f432fa5e31b5885c95b11fdbb46f5f
Domenico Violi <dome.violi90@gmail.com>
2026-03-28 12:02:38 +0100

    update card

--- vurpe-fe/src/app/components/stats-card/stats-card.ts
+++ vurpe-fe/src/app/components/stats-card/stats-card.ts
@@ -1,22 +1,27 @@
-
-import { Component, input} from '@angular/core';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatCardModule} from '@angular/material/card';
-import {MatChipsModule} from '@angular/material/chips';
+import { Component, input } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { CamelToTitlePipe } from '../../pipe/CamelToTitlePipe';
 import { StatusBadgeDirective } from '../../directives/status-badge-directive';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-stats-card',
-   imports: [MatCardModule, MatChipsModule, MatProgressBarModule,CamelToTitlePipe,StatusBadgeDirective],
+  imports: [
+    MatCardModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    CamelToTitlePipe,
+    StatusBadgeDirective,
+    MatIcon,
+  ],
   templateUrl: './stats-card.html',
   styleUrl: './stats-card.scss',
 })
 export class StatsCard {
- list=input.required<any>();
-title = input.required<string>();
+  list = input.required<any>();
+  title = input.required<string>();
 
-constructor() { 
-
-}
+  constructor() {}
 }
